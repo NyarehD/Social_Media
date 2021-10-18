49363683b960d9e10bbe487519b6b6a6edfcaa37
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2021-10-18 14:17:07 +0630

    Added carousel to post images

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,9 @@
 require('./bootstrap');
 $(document).ready(function () {
     $(".toast").toast("show")
+    $(".post-carousel").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false
+    })
 })
