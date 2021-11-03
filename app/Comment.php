161f66c436a638653e45934d58033d8884b015ad
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2021-11-03 20:32:44 +0630

    Fix the issue where query call duplicated relation to user

--- app/Comment.php
+++ app/Comment.php
@@ -7,13 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ["comment"];
-    public $with = ["owner"];
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function owner(){
-        return $this->belongsTo(User::class, "user_id");
-    }
-
 }
