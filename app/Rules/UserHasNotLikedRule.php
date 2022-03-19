1913f4c31fa4bea21b9903ac3c9eb6ec99a5c26d
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-03-19 16:07:15 +0630

    Improve the quality of code

--- app/Rules/UserHasNotLikedRule.php
+++ app/Rules/UserHasNotLikedRule.php
@@ -6,20 +6,16 @@ use App\Like;
 use Auth;
 use Illuminate\Contracts\Validation\Rule;
 
-class UserHasNotLikedRule implements Rule
-{
-    public function __construct(){
+class UserHasNotLikedRule implements Rule {
+    public function __construct() {
         //
     }
 
-    public function passes($attribute, $value): bool{
-        if (Like::where("user_id", Auth::id())->where("post_id", $value)->count() == 0) {
-            return true;
-        }
-        return false;
+    public function passes($attribute, $value): bool {
+        return Like::where("user_id", Auth::id())->where("post_id", $value)->count() === 0;
     }
 
-    public function message(): string{
+    public function message(): string {
         return 'You have already liked the post';
     }
 }
